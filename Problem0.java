edeeb89770a1f6f417f261709d35d903500cfee3
hen43 <whshle08@gmail.com>
2024-09-10 10:40:02 -0700

    more commit
    on ohio fr

--- Problem0.java
+++ Problem0.java
@@ -3,10 +3,10 @@ public class Problem0
   public static void main(String[] args)
   {
     double shtinkers = 3;
-    System.out.println(shtinkers);
+    System.out.println(shtinkers); //3.0
     
     int gronklers = 5.4;
-    System.out.println(gronklers);
+    System.out.println(gronklers); //incompatible types (stinky binky)
 
     // It is not possible to store a double as an int. However, it is possible to store an int as a double.
   }
